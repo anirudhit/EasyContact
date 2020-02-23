d1b3524ea5352fe893c064f34b5ad6a2463b9cf8
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-23 13:34:26 -0600

    Update Employee.java
    - Updated fields
    - Added dateOfJoining, dateOfBirth, skillSet

--- src/com/banirudh/employees/model/Employee.java
+++ src/com/banirudh/employees/model/Employee.java
@@ -3,44 +3,56 @@ package com.banirudh.employees.model;
 public class Employee {
 	private int empid;
 	private String empname;
-	private String salary;
+	private String dateOfJoining;
+	private String dateOfBirth;
+	private String skillSet;
 	
-	
-	public Employee(int empid, String empname, String salary) {
+	public Employee(int empid, String empname, String dateOfJoining, String dateOfBirth, String skillSet) {
 		this.empid = empid;
 		this.empname = empname;
-		this.salary = salary;
+		this.dateOfJoining = dateOfJoining;
+		this.dateOfBirth = dateOfBirth;
+		this.skillSet = skillSet;
 	}
-
 
 	public int getEmpid() {
 		return empid;
 	}
 
-
 	public void setEmpid(int empid) {
 		this.empid = empid;
 	}
-
 
 	public String getEmpname() {
 		return empname;
 	}
 
-
 	public void setEmpname(String empname) {
 		this.empname = empname;
 	}
 
-
-	public String getSalary() {
-		return salary;
+	public String getDateOfJoining() {
+		return dateOfJoining;
 	}
 
-
-	public void setSalary(String salary) {
-		this.salary = salary;
+	public void setDateOfJoining(String dateOfJoining) {
+		this.dateOfJoining = dateOfJoining;
 	}
-	
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getSkillSet() {
+		return skillSet;
+	}
+
+	public void setSkillSet(String skillSet) {
+		this.skillSet = skillSet;
+	}
 	
 }
