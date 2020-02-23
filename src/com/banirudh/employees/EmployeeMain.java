8262a68d4fdfd5ba93dd8c8b2a9df90b48f27b68
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-23 17:10:34 -0600

    Update EmployeeMain.java
    - Updated EmployeesController call

--- src/com/banirudh/employees/EmployeeMain.java
+++ src/com/banirudh/employees/EmployeeMain.java
@@ -1,12 +1,19 @@
 package com.banirudh.employees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.banirudh.employees.controller.EmployeesController;
+import com.banirudh.employees.model.Employee;
 
 public class EmployeeMain {
 
 	public static void main(String[] args) {
 		
-		EmployeesController.getEmployees();
+		List<Employee> empList = new ArrayList<Employee>();
+		
+		empList = EmployeesController.getEmployees();
+		System.out.println(empList);
 		
 	}
 
