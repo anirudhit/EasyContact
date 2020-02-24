10ddb014e525da090eff45fad4aa1da1931ce113
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-24 15:59:27 -0600

    Updated EasyContact
    - Added put and delete methods
    - Added global styles

--- WebContent/js/global.fn.js
+++ WebContent/js/global.fn.js
@@ -41,3 +41,22 @@ $(document).ready(function(){
 	    format: "mmm dd, yyyy"    
 	});
 });
+
+// Initialize $put and $delete
+jQuery.each( [ "put", "delete" ], function( i, method ) {
+  jQuery[ method ] = function( url, data, callback, type ) {
+    if ( jQuery.isFunction( data ) ) {
+      type = type || callback;
+      callback = data;
+      data = undefined;
+    }
+
+    return jQuery.ajax({
+      url: url,
+      type: method,
+      dataType: type,
+      data: data,
+      success: callback
+    });
+  };
+});
