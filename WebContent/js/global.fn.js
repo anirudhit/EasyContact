4bb10bf1c1c9702f93c5e6e115f79cd2689217f5
anirudh bellamkonda <anirudhit@outlook.com>
2020-02-24 14:47:43 -0600

    Create global.fn.js
    - Created file for global function
    - serializeObject for the forms, serailizing chips data, initalize date picker

---
+++ WebContent/js/global.fn.js
@@ -0,0 +1,43 @@
+// Global method to serialize the form input
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+function serializeChips(chipsData){
+	var chips = null;
+	for(chip of chipsData){
+		if(chips)
+			chips = chips + ","+ chip['tag'];
+		else
+			chips = chip['tag'];
+	}
+	return chips?chips:"";
+}
+
+// Initialize Materialize date picker
+$(document).ready(function(){
+	var today			=	new Date();
+	var currentDay		=	today.getDate();
+	var currentMonth	=	today.getMonth()+1;
+	var currentYear		=	today.getFullYear();
+	$(".datepicker").datepicker({
+	    defaultDate: new Date(),
+	    maxDate: new Date(currentYear,currentMonth,currentDay),
+	    yearRange: [1928, currentYear],
+	    format: "mmm dd, yyyy"    
+	});
+});
